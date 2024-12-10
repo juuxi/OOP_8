458d254efd4534478f418e185a4e140c4be7e6e5
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-12-10 13:56:41 +0300

    very small and cutted at 200 sin

--- sample.cpp
+++ sample.cpp
@@ -1,5 +1,6 @@
 #include "sample.h"
 #include <math.h>
+#include <QPainterPath>
 
 TSample::TSample(int n)
 {
@@ -8,17 +9,20 @@ TSample::TSample(int n)
 
 void TSample::draw(QPainter* p, QRect r, QColor c)
 {
-    qreal cw = 0.5*r.width();
-    qreal ch = 0.5*r.height();
-    qreal cr = 0.9*(cw>ch?ch:cw);
-    qreal a = 2.0*acos(-1.0)/count;
-    QPointF *t = new QPointF[count];
-    for (int i=0; i<count; i++)
-    {
-        t[i] = QPointF(100 + cw+cr*sin(i*a),ch-cr*cos(i*a));
-    }
+//    qreal cw = 0.5*r.width();
+//    qreal ch = 0.5*r.height();
+//    qreal cr = 0.9*(cw>ch?ch:cw);
+//    qreal a = 2.0*acos(-1.0)/count;
+//    QPointF *t = new QPointF[count];
+//    for (int i=0; i<count; i++)
+//    {
+//        t[i] = QPointF(100 + cw+cr*sin(i*a),ch-cr*cos(i*a));
+//    }
     p->setPen(QPen(Qt::black));
-    p->setBrush(QBrush(c));
-    p->drawPolygon(t,count);
-    delete [] t;
+//    p->drawPolygon(t,count);
+//    delete [] t;
+    for(int xCoord = 100; xCoord < 200; xCoord++)
+    {
+        p->drawPoint(xCoord, 300 + sin(xCoord - 100));
+    }
 }
